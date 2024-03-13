bea99b203f6289b98ae7443a2708964778edbbbf
Harish Meena <harishmbmit@gmail.com>
2024-03-13 15:08:59 +0530

    change value

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -20,13 +20,13 @@ export default function Home() {
 
       <Image src="/assets/images/hero.png"
       alt="hero"
-      width={10000}
-      height={10000}
+      width={1000}
+      height={1000}
       className="max-h-[70vh] object-contain object-center 2xl:max-h-[50vh]"
       />
     </section>
     
-    <section id="event" className="wrapper my-8 flex flex-col gap-8 md:gap-12">
+    <section id="events" className="wrapper my-8 flex flex-col gap-8 md:gap-12">
     <h2 className="h2-bold">Trust by <br /> Thousands of Events</h2>
 
     <div className="flex w-full flex-col gap-5 md:flex-row">
